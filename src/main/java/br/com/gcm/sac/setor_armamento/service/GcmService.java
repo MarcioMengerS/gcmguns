65797e79da0fc2ad283d336b5108366869efdb78
MarcioMengerS <gm717gem@gmail.com>
2022-10-15 18:06:29 -0300

    adicionada configuração CORS

--- src/main/java/br/com/gcm/sac/setor_armamento/service/GcmService.java
+++ src/main/java/br/com/gcm/sac/setor_armamento/service/GcmService.java
@@ -1,7 +1,6 @@
 package br.com.gcm.sac.setor_armamento.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
