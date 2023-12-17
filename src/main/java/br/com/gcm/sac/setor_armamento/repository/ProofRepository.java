1f709f212afa667c160e3e9a8a4499de0f3a6b46
MarcioMengerS <gm717gem@gmail.com>
2023-12-17 18:13:16 -0300

    adicionado classe para uploads de arquivos

---
+++ src/main/java/br/com/gcm/sac/setor_armamento/repository/ProofRepository.java
@@ -0,0 +1,9 @@
+package br.com.gcm.sac.setor_armamento.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.gcm.sac.setor_armamento.model.Proof;
+
+public interface ProofRepository extends JpaRepository<Proof, Integer>{
+    
+}
