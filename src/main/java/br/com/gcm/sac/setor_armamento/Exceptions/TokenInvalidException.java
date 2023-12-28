9fb1f03dc37ed670d3e8d6ffa27903ee045be555
MarcioMengerS <gm717gem@gmail.com>
2023-12-28 17:29:59 -0300

    Excluida pasta MD5 e alterado padrão da pasta exceptions

--- src/main/java/br/com/gcm/sac/setor_armamento/Exceptions/TokenInvalidException.java
+++ src/main/java/br/com/gcm/sac/setor_armamento/Exceptions/TokenInvalidException.java
@@ -1,4 +1,4 @@
-package br.com.gcm.sac.setor_armamento.Exceptions;
+package br.com.gcm.sac.setor_armamento.exceptions;
 
 public class TokenInvalidException extends RuntimeException{
     public TokenInvalidException(String message){
